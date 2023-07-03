b9a9b373c2328fe4e457efffd294977398577cd7
jeff greenberg <jeff@ziligy.com>
2023-07-03 14:07:43 -0400

    fixed dependency

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "KalmanFilter",
-            dependencies: []),
+            dependencies: [Surge]),
         .testTarget(
             name: "KalmanFilterTests",
             dependencies: ["KalmanFilter"]),
