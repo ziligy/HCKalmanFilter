5e3e60b1dbf6b082006f19c7974a06127ed2b27d
jeff greenberg <jeff@ziligy.com>
2023-07-03 14:11:31 -0400

    fixed dependency

--- Tests/KalmanFilterTests/KalmanFilterTests.swift
+++ Tests/KalmanFilterTests/KalmanFilterTests.swift
@@ -6,6 +6,6 @@ final class KalmanFilterTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(KalmanFilter().text, "Hello, World!")
+//        XCTAssertEqual(KalmanFilter().text, "Hello, World!")
     }
 }
